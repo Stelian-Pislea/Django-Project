32cd53bcd6e0bd63f74754f5fa27cca94c82a228
Stelian-Pislea <stelian.pislea@gmail.com>
2023-05-05 22:03:43 +0300

    modify models

--- django_app/my_site/models.py
+++ django_app/my_site/models.py
@@ -8,9 +8,16 @@ from datetime import datetime
 class Room(models.Model):
     name = models.CharField(max_length=250)
 
+    def __str__(self):
+        return self.name
+
 
 class Message(models.Model):
     value = models.CharField(max_length=250)
     date = models.DateTimeField(default=datetime.now, blank=True)
     user = models.CharField(max_length=250)
     room = models.CharField(max_length=250)
+
+
+    def __str__(self):
+        return f'{self.user} -> {self.value[:15]} ...'
